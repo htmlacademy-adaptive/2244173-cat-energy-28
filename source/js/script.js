4fd22e5c28be43ea1250c6405006e7de03a24cf2
Алина <alinamurtazina777@yandex.ru>
2023-05-14 14:09:26 +0300

    makes a cat slider

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,32 @@ headerToggle.addEventListener('click', function () {
     mainHeader.classList.remove('page-header--opened');
   }
 });
+
+document.documentElement.lang = "ru";
+document.documentElement.classList.add("page");
+
+const slider = document.querySelector(".slider");
+const curtain = slider.querySelector(".slider__curtain");
+let sliderStyles = getComputedStyle(slider);
+let curtainPlaceStart
+let clientX
+
+window.addEventListener("pointerup", stopTheCurtainShifting);
+curtain.addEventListener("pointerdown", startTheCurtainShifting);
+
+function startTheCurtainShifting (event) {
+curtainPlaceStart = +(sliderStyles.getPropertyValue("--curtain-place"))
+clientX = event.clientX
+window.addEventListener("pointermove", shiftТheСurtain)
+}
+
+function shiftТheСurtain (event) {
+let deltaX = event.clientX - clientX
+let cursorPlace = curtainPlaceStart + deltaX / slider.clientWidth
+let curtainPlace = Math.min(Math.max(cursorPlace, 0), 1)
+slider.style.setProperty("--curtain-place", `${curtainPlace}`)
+}
+
+function stopTheCurtainShifting () {
+window.removeEventListener("pointermove", shiftТheСurtain);
+}
