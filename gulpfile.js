b99d9333209eedd4e77be457ea34658a856995d1
Алина <alinamurtazina777@yandex.ru>
2023-05-17 12:40:33 +0300

    puts a favicon

--- gulpfile.js
+++ gulpfile.js
@@ -69,11 +69,11 @@ const createWebp = () => {
 
 // SVG
 
-/* const svg = () => {
+const svg = () => {
   return gulp.src('source/favicons/*.svg')
     .pipe(svgo())
     .pipe(gulp.dest('build/img/favicons'))
-} */
+}
 
 const makeStack = () => {
   return gulp.src('source/img/*.svg')
@@ -87,7 +87,7 @@ const makeStack = () => {
 const copy = (done) => {
   gulp.src([
     'source/fonts/**/*.{woff2,woff}',
-    /* 'source/*.ico', */
+    'source/*.ico',
     'source/img/logo-mobile.svg',
     'source/img/logo-tablet.svg',
     'source/img/logo-desktop.svg',
@@ -147,6 +147,7 @@ export const build = gulp.series(
     html,
     scripts,
     makeStack,
+    svg,
     createWebp
   ),
 );
@@ -162,6 +163,7 @@ export default gulp.series(
     html,
     scripts,
     makeStack,
+    svg,
     createWebp
   ),
   gulp.series(
